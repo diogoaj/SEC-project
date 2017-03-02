f4498a20fefdf3c123904b871ddad0c4a0e9b365
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2017-03-02 22:26:39 +0000

    Move script

--- src/main/server/PasswordServer.java
+++ src/main/server/PasswordServer.java
@@ -7,23 +7,22 @@ import java.rmi.server.UnicastRemoteObject;
 import java.security.Key;
 
 public class PasswordServer implements InterfaceRMI{
-
-	@Override
+	
 	public void register(Key publicKey) throws RemoteException {
 		// TODO Auto-generated method stub
+		
 	}
 
-	@Override
 	public void put(Key publicKey, byte[] domain, byte[] username, byte[] password) throws RemoteException {
 		// TODO Auto-generated method stub
+		
 	}
 
-	@Override
 	public byte[] get(Key publicKey, byte[] domain, byte[] username) throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		try{
 			PasswordServer s = new PasswordServer();
@@ -38,6 +37,5 @@ public class PasswordServer implements InterfaceRMI{
 			e.printStackTrace();
 		}
 	}
-
 
 }
