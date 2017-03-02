de2a153cdb177495c17b9f5746b029e788b6e5aa
Ricardo <ricardo.g.pereira@ist.utl.pt>
2017-03-02 22:30:52 +0000

    client started

--- src/main/client/Client.java
+++ src/main/client/Client.java
@@ -1,5 +1,23 @@
 package main.client;
 
-public class Client{
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import main.server
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		String host = (args.length < 1) ? null : args[0];
+
+		try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            InterfaceRMI stub = (InterfaceRMI) registry.lookup("Interface");
+            System.out.println("stub found");
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+	}
 
 }
