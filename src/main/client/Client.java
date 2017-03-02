7fd949887eb26718a106d18731a380ac3d22a8c1
Ricardo <ricardo.g.pereira@ist.utl.pt>
2017-03-02 22:33:11 +0000

    small fix

--- src/main/client/Client.java
+++ src/main/client/Client.java
@@ -2,22 +2,21 @@ package main.client;
 
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
-import main.server
+import main.server.InterfaceRMI;
 
 public class Client {
 
 	public static void main(String[] args) {
 		
 		String host = (args.length < 1) ? null : args[0];
-
+		
 		try {
-            Registry registry = LocateRegistry.getRegistry(host);
-            InterfaceRMI stub = (InterfaceRMI) registry.lookup("Interface");
-            System.out.println("stub found");
+        	Registry registry = LocateRegistry.getRegistry(host);
+        	InterfaceRMI stub = (InterfaceRMI) registry.lookup("Interface");
+        	System.out.println("stub found");
         } catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
-        }
+        	System.err.println("Client exception: " + e.toString());
+        	e.printStackTrace();
+    	}
 	}
-
 }
