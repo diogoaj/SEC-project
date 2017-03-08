92d645a0abe00477ed46a69a1f751d1e155cb3c2
Ricardo <ricardo.g.pereira@ist.utl.pt>
2017-03-08 23:12:48 +0000

    Signature functions added

--- Interface/src/main/InterfaceRMI.java
+++ Interface/src/main/InterfaceRMI.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 import java.security.Key;
 
 public interface InterfaceRMI extends Remote {
-    void register(Key publicKey) throws RemoteException;
+    void register(Key publicKey, byte[] signedData) throws RemoteException;
     void put(Key publicKey, byte[] domain, byte[] username, byte[] password) throws RemoteException;
     byte[] get(Key publicKey, byte[] domain, byte[] username) throws RemoteException;
 }
