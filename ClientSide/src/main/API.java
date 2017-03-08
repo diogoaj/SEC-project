d036143dfa6983d3947b3431be706d9d7747c7b7
Ricardo <ricardo.g.pereira@ist.utl.pt>
2017-03-08 00:47:51 +0000

    API methods implemented without security (not tested because I have some problems with IDE)

--- ClientSide/src/main/API.java
+++ ClientSide/src/main/API.java
@@ -3,7 +3,9 @@ package main;
 import java.io.FileInputStream;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.security.KeyStore;;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;;
 
 public class API {
 
@@ -27,19 +29,60 @@ public class API {
 	}
 	
 	public void register_user(){
-		
+		try{
+			stub.register(getPublicKey());
+		}
+		catch(Exception e){
+			System.err.println("Register user exception: " + e.toString());
+        	e.printStackTrace();
+		}
 	}
 	
 	public void save_password(byte[] domain, byte[] username, byte[] password){
-		
+		try{
+			stub.put(getPublicKey(), domain, username, password);
+		}
+		catch(Exception e){
+			System.err.println("Register user exception: " + e.toString());
+        	e.printStackTrace();
+		}
 	}
 	
 	public byte[] retrieve_password(byte[] domain, byte[] username){
-		return "dummy".getBytes();
+		try{
+			return stub.get(getPublicKey(), domain, username);
+		}
+		catch(Exception e){
+			System.err.println("Register user exception: " + e.toString());
+        	e.printStackTrace();
+		}
+		return null;
 	}
 	
 	public void close(){
 		System.exit(0);
+	}
+	
+	private PublicKey getPublicKey(){
+		try{
+			return keyStore.getCertificate("clientkeystore").getPublicKey();
+		}
+		catch(Exception e){
+			System.err.println("Register user exception: " + e.toString());
+        	e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private PrivateKey getPrivateKey(){
+		try{
+			return (PrivateKey)keyStore.getKey("clientkeystore", password.toCharArray());
+		}
+		catch(Exception e){
+			System.err.println("Register user exception: " + e.toString());
+        	e.printStackTrace();
+		}
+		return null;
 	}
 	
 }
